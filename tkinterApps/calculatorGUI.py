f7698f941a18b92d54c09463002868c0ef56c1d0
takuHP <tzhira8@gmail.com>
2024-03-13 04:01:14 +0200

    updated but looks ugly. pliz fix

--- tkinterApps/calculatorGUI.py
+++ tkinterApps/calculatorGUI.py
@@ -7,36 +7,29 @@ base1 = Tk()
 
 base1.title("Calculator")
 
-entry_field1 = Entry()
-entry_field2 = Entry()
-result_label = Label(text=f" ")
-
 topFrame = Frame(base1)
-display1 = Text(topFrame, background="grey", foreground="green",name="i fill up this space").grid(row=1, column=1)
+result_label= Label(topFrame, text="")
+firstNum = Entry(topFrame)
+secNum =Entry(topFrame)
 
 bottomFrame = Frame(base1)
-#clearScreen = Button(bottomFrame, text="C", background="red")
-#divideSign = Button(bottomFrame, text="/",background="red")
-#percentageSign = Button(bottomFrame, text="%",background="red")
-#times_Sign = Button(bottomFrame, text="*",background="red")
-#equals_Sign = Button(bottomFrame, text="=").grid(row=7, column=2)
+#addition etc logic and positioning
+add_button =Button(bottomFrame, text="+", background="red",command=lambda: calculate("add"))
+subtract_button =Button(bottomFrame, text="-", background="red", command=lambda: calculate("subtract"))
+multiply_button =Button(bottomFrame, text="*", background="red", command=lambda: calculate("multiply"))
+divide_button =Button(bottomFrame, text="/", background="red",command=lambda: calculate("divide"))
+
 for i in range(0,10,1):
     Button(bottomFrame, text=str(i) ).grid( column=3 if i%3==0 else (1 if i%3==1 else 2), row= 4 if i<=3 else (5 if i<=6 else 6))
 numZero = Button(bottomFrame, text="0").grid(column=3, row=7)
 
-topFrame.grid(row=1, column=1)
-bottomFrame.grid(row=2, column=1, rowspan=7 , sticky=W)
-
-#addition etc logic and positioning
-add_button =Button(base1, text="+", background="red",command=lambda: calculate("add")).grid(row=7, column=1, sticky=W)
-subtract_button =Button(base1, text="-", background="red", command=lambda: calculate("subtract")).grid(row=2, column=2, sticky=W)
-multiply_button =Button(base1, text="*", background="red", command=lambda: calculate("multiply")).grid(row=2, column=3, sticky=W)
-divide_button =Button(bottomFrame, text="/", background="red",command=lambda: calculate("divide")).grid(row=2, column=1, sticky=W)
+topFrame.grid(row=1, column=1, sticky=W)
+bottomFrame.grid(row=2, column=1 , sticky=W)
 
 #logic funtions
 def calculate(operation):
-    num1 = float(entry_field1.get())
-    num2 = float(entry_field2.get())
+    num1 = float(firstNum.get())
+    num2 = float(secNum.get())
 
     if operation == "add":
         result = num1 + num2
@@ -44,7 +37,7 @@ def calculate(operation):
         result = num1 - num2
     elif operation == "divide":
         if num2 == 0:
-            pass
+            result = "Can't divide by 0"
         else:
             result = float(num1 / num2)
     elif operation == "multiply":
@@ -52,6 +45,16 @@ def calculate(operation):
     
     result_label.config(text=f"Result: {result}")
 
+#aesthetics
+    #topFrame
+result_label.grid(row=3, column = 2, columnspan=2)
+firstNum.grid(row=1, column=1)
+secNum.grid(row=1, column=3, padx=10)
 
+    #bottomFrame
+add_button.grid(row=7, column=1, sticky=W)
+subtract_button.grid(row=2, column=2, sticky=W)
+multiply_button.grid(row=2, column=3, sticky=W)
+divide_button.grid(row=2, column=1, sticky=W)
 
 base1.mainloop()
